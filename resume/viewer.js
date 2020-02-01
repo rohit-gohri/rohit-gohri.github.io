ddbba8b6e557dd70c7cf0dfb72bc1369c6d9f478
Rohit Gohri <gohri.rohit@gmail.com>
2020-02-02 00:06:11 +0530

    Update links and gh-pages step

--- resume/viewer.js
+++ resume/viewer.js
@@ -1730,7 +1730,7 @@ var PDFViewerApplication = {
 exports.PDFViewerApplication = PDFViewerApplication;
 var validateFileURL;
 {
-  var HOSTED_VIEWER_ORIGINS = ['null', 'http://localhost:3000', 'http://rohit-smpx.github.io', 'https://rohit-smpx.github.io', 'http://rohit.boring.download', 'https://rohit.boring.download'];
+  var HOSTED_VIEWER_ORIGINS = ['null', 'http://localhost:3000', 'http://rohit-gohri.github.io', 'https://rohit-gohri.github.io', 'http://rohit.boring.download', 'https://rohit.boring.download'];
 
   validateFileURL = function validateFileURL(file) {
     if (file === undefined) {
